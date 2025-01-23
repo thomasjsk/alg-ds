69490ee1b0cc6bea8b488614a96dbcd924606a87
thomasjsk <thomas.jasiuk@gmail.com>
2025-01-23 10:50:04 +0100

    Array list push_front, pop_front

--- src/array_list.rs
+++ src/array_list.rs
@@ -65,6 +65,42 @@ where
         item
     }
 
+    pub fn pop_front(&mut self) -> Option<T> {
+        if self.len == 0 {
+            return None;
+        }
+
+        let item = unsafe { Some(ptr::read(self.array.add(0))) };
+        for i in 0..self.len {
+            unsafe {
+                ptr::write(self.array.add(i), ptr::read(self.array.add(i + 1)));
+            }
+        }
+
+        self.len -= 1;
+        self.cap -= 1;
+
+        unsafe {
+            ptr::drop_in_place(self.array.add(self.len));
+        }
+
+        item
+    }
+
+    pub fn push_front(&mut self, value: T) {
+        if self.len == self.cap {
+            self.resize(self.cap * 2);
+        }
+
+        for i in (0..self.len).rev() {
+            unsafe { ptr::write(self.array.add(i + 1), ptr::read(self.array.add(i))) };
+        }
+
+        unsafe { ptr::write(self.array.add(0), value) };
+
+        self.len += 1;
+    }
+
     fn with_capacity(cap: usize) -> *mut T {
         if cap == 0 {
             return ptr::null_mut();
@@ -159,5 +195,63 @@ mod tests {
         list.push(6);
         assert_eq!(list.pop(), Some(6));
         assert_eq!(list.print(), &[1, 2, 4, 5, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn pop_front() {
+        let initial_cap = 3;
+        let mut list = ArrayList::<u32>::new(initial_cap);
+        assert_eq!(list.pop_front(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.print(), &[2, 3]);
+
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.print(), &[3]);
+
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.print(), &[]);
+
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.print(), &[]);
+    }
+
+    #[test]
+    fn push_front() {
+        let initial_cap = 3;
+        let mut list = ArrayList::<u32>::new(initial_cap);
+        assert_eq!(list.len(), 0);
+
+        list.push_front(1);
+        assert_eq!(list.print(), &[1, 0, 0]);
+
+        list.push_front(2);
+        assert_eq!(list.print(), &[2, 1, 0]);
+
+        list.push_front(3);
+        assert_eq!(list.print(), &[3, 2, 1]);
+
+        list.push_front(4);
+        assert_eq!(list.print(), &[4, 3, 2, 1, 0, 0]);
+    }
+
+    #[test]
+    fn push_front_and_back() {
+        let initial_cap = 4;
+        let mut list = ArrayList::<u32>::new(initial_cap);
+
+        list.push_front(1);
+        assert_eq!(list.print(), &[1, 0, 0, 0]);
+
+        list.push(2);
+        assert_eq!(list.print(), &[1, 2, 0, 0]);
+
+        list.push_front(3);
+        assert_eq!(list.print(), &[3, 1, 2, 0]);
+
+        list.push(4);
+        assert_eq!(list.print(), &[3, 1, 2, 4]);
     }
 }
